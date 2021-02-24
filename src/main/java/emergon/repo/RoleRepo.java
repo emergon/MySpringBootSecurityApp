b4b598ccaf0a3f158314f28f70968d776eeceaf7
Anastasios Lelakis <tasos@hotmail.com>
2021-02-24 20:40:42 +0200

    add Query

--- src/main/java/emergon/repo/RoleRepo.java
+++ src/main/java/emergon/repo/RoleRepo.java
@@ -2,10 +2,13 @@ package emergon.repo;
 
 import emergon.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RoleRepo extends JpaRepository<Role, Integer> {
     
-    public Role findByRname(String rname);
+    @Query("SELECT r FROM Role r WHERE r.rname = :rname")
+    public Role findByRname(@Param("rname")String rname);
 }
